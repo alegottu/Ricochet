46fb02e66df49b79e65e07c113cd288e95e789d0
alegottu <33403572+alegottu@users.noreply.github.com>
2021-08-13 01:50:56 -0700

    General enemy spawner improvements

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -1,70 +1,58 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject[] enemyPool = null;
-    [SerializeField] private int[] waveAmounts = null;
-    // The number at x index should correspond to the difficulty multiplier at which the enemy at x index should be introduced to the pool.
-    [SerializeField] private int[] enemyAmounts = null;
-    // Each index should correspond to the amount of enemies to be destroyed before the difficulty increases
+    [SerializeField] private GameObject[] enemyPrefabs = null;
     [SerializeField] private Health player = null;
-    [SerializeField] private float spawningRange = 0;
-    [SerializeField] private float bufferSize = 0;
-    [SerializeField] private float spawnCooldown = 0;
-    [SerializeField] private float spawnRateDecrease = 1;
-    [SerializeField] private float difficultyMultiplier = 1;
+    [SerializeField] private SpawnerData data = null;
 
+    private float difficultyMultiplier = 1;
+    private float spawnCooldown = 0;
     private int poolSize = 1;
     private int deaths = 0;
-    private float spawnTimer = 0;
-    private Vector3 currentEnemyPos = Vector3.zero;
 
-    private Vector3 SpawnEnemy(Vector3 previous)
+    private void Awake()
     {
-        Vector3 position = transform.position + Vector3.right * Random.Range(-spawningRange, spawningRange);
-        while (position.x >= previous.x - bufferSize && position.x <= previous.x + bufferSize)
+        spawnCooldown = data.baseSpawnRate;
+        StartCoroutine(SpawnEnemies());
+    }
+
+    private Vector3 SpawnEnemy(Vector3 previousEnemyPos)
+    {
+        Vector3 position = transform.position + Vector3.right * Random.Range(-data.spawningRange, data.spawningRange);
+        while (position.x >= previousEnemyPos.x - data.spawnBufferSize && position.x <= previousEnemyPos.x + data.spawnBufferSize)
         {
-            position = transform.position + Vector3.right * Random.Range(-spawningRange, spawningRange);
+            position = transform.position + Vector3.right * Random.Range(-data.spawningRange, data.spawningRange);
         }
 
-        GameObject currentEnemy = Instantiate(enemyPool[Random.Range(0, poolSize)], position, Quaternion.identity);
+        GameObject currentEnemy = Instantiate(enemyPrefabs[Random.Range(0, poolSize)], position, Quaternion.identity);
         currentEnemy.GetComponent<Enemy>().SetUp(player, difficultyMultiplier);
         currentEnemy.GetComponent<Health>().OnDeath += OnEnemyDeathEventHandler;
         
         return position;
     }
 
+    private IEnumerator SpawnEnemies()
+    {
+        Vector3 currentEnemyPos = Vector3.zero;
+
+        while (true)
+        {
+            currentEnemyPos = SpawnEnemy(currentEnemyPos);
+            yield return new WaitForSeconds(spawnCooldown);
+            spawnCooldown -= data.spawnRateDecrease;
+            difficultyMultiplier += data.difficultyMultiplierIncrease;
+        }
+    }
+
     private void OnEnemyDeathEventHandler()
     {
         deaths++;
-    }
 
-    private void IncreaseDifficulty()
-    {
-        difficultyMultiplier++;
-        
-        if (waveAmounts[poolSize - 1] * poolSize == difficultyMultiplier)
+        if (deaths >= data.baseDeathsThreshold * difficultyMultiplier)
         {
             poolSize++;
-        }
-
-        spawnCooldown -= spawnRateDecrease;
-    }
-
-    private void Update()
-    {
-        spawnTimer -= Time.deltaTime;
-
-        if (spawnTimer <= 0)
-        {
-            currentEnemyPos = SpawnEnemy(currentEnemyPos);
-            spawnTimer = spawnCooldown;
-        }
-
-        if (deaths >= enemyAmounts[(int)difficultyMultiplier - 1])
-        {
-            deaths = 0;
-            IncreaseDifficulty();
         }
     }
 }
---
+++ Assets/Scripts/Enemies/SpawnerData.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Spawner Data", menuName = "Spawner Data", order = 2)]
+public class SpawnerData : ScriptableObject
+{
+    [SerializeField] private int _baseDeathsThreshold = 0; // The base amount of enemy deaths required for the difficulty to change
+    public int baseDeathsThreshold { get { return _baseDeathsThreshold; } }
+
+    [SerializeField] private float _spawningRange = 0; // x amount from zero that enemies can spawn within
+    public float spawningRange { get { return _spawningRange; } }
+
+    [SerializeField] private float _spawnBufferSize = 0; // The size that indicates the area which the previous spawn took up
+    public float spawnBufferSize { get { return _spawnBufferSize; } }
+
+    [SerializeField] private float _baseSpawnRate = 0;
+    public float baseSpawnRate { get { return _baseSpawnRate; } }
+
+    [SerializeField] private float _spawnRateDecrease = 0; // The amount by which the spawn rate decreases (in seconds) every cycle
+    public float spawnRateDecrease { get { return _spawnRateDecrease; } }
+
+    [SerializeField] private float _difficultyMultiplierIncrease = 0; // The amount by which the diffculty multiplier increases each cycle
+    public float difficultyMultiplierIncrease { get { return _difficultyMultiplierIncrease; } }
+}
